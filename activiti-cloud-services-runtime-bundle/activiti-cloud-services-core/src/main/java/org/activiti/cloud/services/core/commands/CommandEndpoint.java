ae325baba557870725aa0d7d9175260588576b23
Salaboy <salaboy@gmail.com>
2017-10-30 18:31:52 -0400

    adding separate stream listeners

--- activiti-cloud-services-runtime-bundle/activiti-cloud-services-core/src/main/java/org/activiti/cloud/services/core/commands/CommandEndpoint.java
+++ activiti-cloud-services-runtime-bundle/activiti-cloud-services-core/src/main/java/org/activiti/cloud/services/core/commands/CommandEndpoint.java
@@ -6,6 +6,14 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 import org.activiti.cloud.services.core.model.commands.AbstractCommand;
+import org.activiti.cloud.services.core.model.commands.ActivateProcessInstanceCmd;
+import org.activiti.cloud.services.core.model.commands.ClaimTaskCmd;
+import org.activiti.cloud.services.core.model.commands.CompleteTaskCmd;
+import org.activiti.cloud.services.core.model.commands.ReleaseTaskCmd;
+import org.activiti.cloud.services.core.model.commands.SetTaskVariablesCmd;
+import org.activiti.cloud.services.core.model.commands.SignalProcessInstancesCmd;
+import org.activiti.cloud.services.core.model.commands.StartProcessInstanceCmd;
+import org.activiti.cloud.services.core.model.commands.SuspendProcessInstanceCmd;
 import org.activiti.cloud.services.events.ProcessEngineChannels;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -26,8 +34,46 @@ public class CommandEndpoint {
     }
 
     @StreamListener(ProcessEngineChannels.COMMAND_CONSUMER)
-    public void consumeCommand(AbstractCommand cmd) {
+    public void consumeStartProcessInstanceCmd(StartProcessInstanceCmd cmd) {
+        processCommand(cmd);
+    }
 
+    @StreamListener(ProcessEngineChannels.COMMAND_CONSUMER)
+    public void consumeActivateProcessInstanceCmd(ActivateProcessInstanceCmd cmd) {
+        processCommand(cmd);
+    }
+
+    @StreamListener(ProcessEngineChannels.COMMAND_CONSUMER)
+    public void consumeClaimTaskCmd(ClaimTaskCmd cmd) {
+        processCommand(cmd);
+    }
+
+    @StreamListener(ProcessEngineChannels.COMMAND_CONSUMER)
+    public void consumeCompleteTaskCmd(CompleteTaskCmd cmd) {
+        processCommand(cmd);
+    }
+
+    @StreamListener(ProcessEngineChannels.COMMAND_CONSUMER)
+    public void consumeReleaseTaskCmd(ReleaseTaskCmd cmd) {
+        processCommand(cmd);
+    }
+
+    @StreamListener(ProcessEngineChannels.COMMAND_CONSUMER)
+    public void consumeSetTaskVariableCmd(SetTaskVariablesCmd cmd) {
+        processCommand(cmd);
+    }
+
+    @StreamListener(ProcessEngineChannels.COMMAND_CONSUMER)
+    public void consumeSignalProcessInstancesCmd(SignalProcessInstancesCmd cmd) {
+        processCommand(cmd);
+    }
+
+    @StreamListener(ProcessEngineChannels.COMMAND_CONSUMER)
+    public void consumeSuspendProcessInstanceCmd(SuspendProcessInstanceCmd cmd) {
+        processCommand(cmd);
+    }
+
+    private void processCommand(AbstractCommand cmd) {
         CommandExecutor cmdExecutor = commandExecutors.get(cmd.getClass());
         if (cmdExecutor != null) {
             cmdExecutor.execute(cmd);
