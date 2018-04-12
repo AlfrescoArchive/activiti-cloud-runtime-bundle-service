a5835e616e2115bd228e431ad59671237949fe34
Bassam Al-Sarori <balsarori@gmail.com>
2018-04-12 10:27:27 +0100

    remove unneeded Component annotation

--- activiti-cloud-starter-configure/src/main/java/org/activiti/spring/integration/RuntimeBundleActivityBehaviorFactory.java
+++ activiti-cloud-starter-configure/src/main/java/org/activiti/spring/integration/RuntimeBundleActivityBehaviorFactory.java
@@ -4,9 +4,7 @@ import org.activiti.bpmn.model.ServiceTask;
 import org.activiti.engine.delegate.Expression;
 import org.activiti.engine.impl.bpmn.parser.factory.DefaultActivityBehaviorFactory;
 import org.activiti.engine.impl.delegate.ActivityBehavior;
-import org.springframework.stereotype.Component;
 
-@Component
 public class RuntimeBundleActivityBehaviorFactory extends DefaultActivityBehaviorFactory {
 
     @Override
