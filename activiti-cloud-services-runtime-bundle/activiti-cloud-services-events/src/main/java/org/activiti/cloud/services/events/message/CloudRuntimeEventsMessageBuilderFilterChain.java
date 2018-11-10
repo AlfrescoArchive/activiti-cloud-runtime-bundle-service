c006ed9b83c015659808870125f751914c7df320
Igor Dianov <igor@dianov.com>
2018-11-10 00:23:40 -0800

    fix: codacy warning

--- activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/main/java/org/activiti/cloud/services/events/message/CloudRuntimeEventsMessageBuilderFilterChain.java
+++ activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/main/java/org/activiti/cloud/services/events/message/CloudRuntimeEventsMessageBuilderFilterChain.java
@@ -20,6 +20,7 @@ import org.activiti.cloud.api.model.shared.events.CloudRuntimeEvent;
 public class CloudRuntimeEventsMessageBuilderFilterChain extends MessageBuilderFilterChain<CloudRuntimeEvent<?, ?>[]> {
 
     public CloudRuntimeEventsMessageBuilderFilterChain() {
+        super();
     }
 
 }
