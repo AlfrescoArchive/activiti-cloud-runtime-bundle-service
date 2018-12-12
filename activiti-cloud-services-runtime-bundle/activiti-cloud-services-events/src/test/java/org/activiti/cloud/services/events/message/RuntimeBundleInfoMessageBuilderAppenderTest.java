f697281f8bc6c032bc4a849364b977fd06a4fd11
Igor Dianov <igor@dianov.com>
2018-12-12 00:32:07 -0800

    fix: add RuntimeBundleInfoMessageBuilderAppenderTest

--- activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/test/java/org/activiti/cloud/services/events/message/RuntimeBundleInfoMessageBuilderAppenderTest.java
+++ activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/test/java/org/activiti/cloud/services/events/message/RuntimeBundleInfoMessageBuilderAppenderTest.java
@@ -15,13 +15,57 @@
  */
 package org.activiti.cloud.services.events.message;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.activiti.cloud.api.model.shared.events.CloudRuntimeEvent;
+import org.activiti.cloud.api.model.shared.impl.conf.IgnoredRuntimeEvent;
+import org.activiti.cloud.services.events.configuration.RuntimeBundleProperties;
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 public class RuntimeBundleInfoMessageBuilderAppenderTest {
 
+    private static final String SPRING_APP_NAME = "springAppName";
+    private static final String SERVICE_VERSION = "serviceVersion";
+    private static final String SERVICE_TYPE = "serviceType";
+    private static final String APP_VERSION = "appVersion";
+    private static final String APP_NAME = "appName";
+
+    private RuntimeBundleInfoMessageBuilderAppender subject;
+    
+    
+    @Before
+    public void setUp() {
+           RuntimeBundleProperties properties = new RuntimeBundleProperties();
+           
+           properties.setAppName(APP_NAME);
+           properties.setAppVersion(APP_VERSION);
+           properties.setServiceType(SERVICE_TYPE);
+           properties.setServiceVersion(SERVICE_VERSION);
+           properties.setRbSpringAppName(SPRING_APP_NAME);
+           
+           subject = new RuntimeBundleInfoMessageBuilderAppender(properties);
+    }
+    
     @Test
     public void testApply() {
-        // TODO
+        // given
+        MessageBuilder<CloudRuntimeEvent<?,?>> request = MessageBuilder.withPayload(new IgnoredRuntimeEvent());
+        
+        // when
+        subject.apply(request);
+        
+        // then
+        Message<CloudRuntimeEvent<?,?>> message = request.build();
+        
+        assertThat(message.getHeaders())
+                .containsEntry(RuntimeBundleInfoMessageHeaders.APP_NAME, APP_NAME)
+                .containsEntry(RuntimeBundleInfoMessageHeaders.APP_VERSION, APP_VERSION)
+                .containsEntry(RuntimeBundleInfoMessageHeaders.SERVICE_NAME, SPRING_APP_NAME)
+                .containsEntry(RuntimeBundleInfoMessageHeaders.SERVICE_TYPE, SERVICE_TYPE)
+                .containsEntry(RuntimeBundleInfoMessageHeaders.SERVICE_VERSION, SERVICE_VERSION);
     }
 
 }
