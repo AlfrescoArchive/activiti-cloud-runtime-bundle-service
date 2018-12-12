3eb7f3d1393b0eb8d678947624d207cfdc803c0f
CTI777 <illia.goncharov@googlemail.com>
2018-12-12 13:25:08 +0100

    feat: endpoint for subprocesses method

--- activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/main/java/org/activiti/cloud/services/rest/controllers/ProcessInstanceAdminControllerImpl.java
+++ activiti-cloud-services-runtime-bundle/activiti-cloud-services-rest-impl/src/main/java/org/activiti/cloud/services/rest/controllers/ProcessInstanceAdminControllerImpl.java
@@ -105,5 +105,16 @@ public class ProcessInstanceAdminControllerImpl implements ProcessInstanceAdminC
         return resourceAssembler.toResource(processAdminRuntime.update(payload));
     }
 	
+    @Override
+    public PagedResources<ProcessInstanceResource> subprocesses(@PathVariable String processInstanceId,
+                                                                Pageable pageable) {
+        Page<ProcessInstance> processInstancePage = processAdminRuntime.subprocesses(pageConverter.toAPIPageable(pageable), 
+                                                                                     ProcessPayloadBuilder.subprocesses(processInstanceId));
+                
+              
+        return pagedResourcesAssembler.toResource(pageable,
+                                                  pageConverter.toSpringPage(pageable, processInstancePage),
+                                                  resourceAssembler);
+    }
 
 }
