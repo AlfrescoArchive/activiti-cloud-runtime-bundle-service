cc849b3aafb466d59540860df330f1bfc01eb795
Elias Ricken de Medeiros <elias.demedeiros@alfresco.com>
2019-04-09 15:55:57 +0100

    Produce swagger file as a Maven artifact
    - add plugin configuration to produce swagger file as a Maven artifact
    - produce both Alfresco and Hal format

--- activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/swagger/SwaggerIT.java
+++ activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/swagger/SwaggerIT.java
@@ -16,6 +16,9 @@
 
 package org.activiti.cloud.starter.tests.swagger;
 
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -31,8 +34,6 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-
-import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -68,14 +69,20 @@ public class SwaggerIT {
     }
 
     /**
-     * Generates the swagger.json definition of the service
+     * This is more than a simple test. It's actually generating the swagger.json definition of the service
      */
     @Test
     public void generateSwagger() throws Exception {
         MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
         mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
                 .andDo((result) -> {
-                    FileUtils.writeStringToFile(new File("swagger.json"), result.getResponse().getContentAsString());
+                    FileUtils.writeStringToFile(new File("target/swagger.json"), result.getResponse().getContentAsString(),
+                                                StandardCharsets.UTF_8);
+                });
+        mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs?group=hal").accept(MediaType.APPLICATION_JSON))
+                .andDo((result) -> {
+                    FileUtils.writeStringToFile(new File("target/swagger-hal.json"), result.getResponse().getContentAsString(),
+                                                StandardCharsets.UTF_8);
                 });
     }
 
