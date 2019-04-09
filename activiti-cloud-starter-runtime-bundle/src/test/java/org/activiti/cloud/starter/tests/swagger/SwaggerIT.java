1875f4d1c39baf1f8b7b2f46dbebaf64debaf2f7
Miguel Ruiz <miguel.ruiz.developer@gmail.com>
2019-04-09 11:28:50 +0100

    Add swagger generator test

--- activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/swagger/SwaggerIT.java
+++ activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/swagger/SwaggerIT.java
@@ -16,15 +16,23 @@
 
 package org.activiti.cloud.starter.tests.swagger;
 
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.io.File;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -35,6 +43,9 @@ public class SwaggerIT {
 
     @Autowired
     private TestRestTemplate testRestTemplate;
+
+    @Autowired
+    private WebApplicationContext context;
 
     @Test
     public void defaultSpecificationFileShouldBeAlfrescoFormat() {
@@ -54,6 +65,18 @@ public class SwaggerIT {
                 .doesNotContain("PagedResources«")
                 .doesNotContain("Resources«Resource«")
                 .doesNotContain("Resource«");
+    }
+
+    /**
+     * Generates the swagger.json definition of the service
+     */
+    @Test
+    public void generateSwagger() throws Exception {
+        MockMvc mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
+                .andDo((result) -> {
+                    FileUtils.writeStringToFile(new File("swagger.json"), result.getResponse().getContentAsString());
+                });
     }
 
 }
