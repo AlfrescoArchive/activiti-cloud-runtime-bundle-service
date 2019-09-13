bbc1858116bdce346ed832e69a108a36762422e5
Igor Dianov <igor@dianov.com>
2019-09-12 19:56:07 -0700

    fix: convert ITs to unit tests

--- example-runtime-bundle/src/test/java/org/activiti/cloud/runtime/RuntimeBundleApplicationTest.java
+++ example-runtime-bundle/src/test/java/org/activiti/cloud/runtime/RuntimeBundleApplicationTest.java
@@ -1,9 +1,7 @@
 package org.activiti.cloud.runtime;
 
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -11,7 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = RuntimeBundleApplication.class)
 @DirtiesContext
-public class RuntimeBundleApplicationIT {
+public class RuntimeBundleApplicationTest {
 
     @Test
     public void contextLoads() throws Exception {
