137de60cf421563ba13f6fc35a1b0b6d05146ed5
Igor Dianov <igor@dianov.com>
2019-12-16 19:11:32 -0800

    refactor: Removed Message Services Aggregator and Starter modules

--- activiti-cloud-services-runtime-bundle/messages-aggregator/src/main/java/org/activiti/cloud/services/messages/aggregator/advice/MessageConnectorHandlerAdvice.java
+++
@@ -1,7 +0,0 @@
-package org.activiti.cloud.services.messages.aggregator.advice;
-
-import org.springframework.integration.handler.advice.HandleMessageAdvice;
-
-public interface MessageConnectorHandlerAdvice extends HandleMessageAdvice {
-
-}
