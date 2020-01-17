2ea0bf54c83ea9c8ec10208823997dd383c0a246
Igor Dianov <igor@dianov.com>
2020-01-16 23:36:11 -0800

    fix(AAE-731): Added root execution context message headers support

--- activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/main/java/org/activiti/cloud/services/events/listeners/ProcessEngineEventsAggregator.java
+++ activiti-cloud-services-runtime-bundle/activiti-cloud-services-events/src/main/java/org/activiti/cloud/services/events/listeners/ProcessEngineEventsAggregator.java
@@ -16,6 +16,8 @@
 
 package org.activiti.cloud.services.events.listeners;
 
+import static org.activiti.cloud.services.events.listeners.MessageProducerCommandContextCloseListener.ROOT_EXECUTION_CONTEXT;
+
 import org.activiti.api.model.shared.model.VariableInstance;
 import org.activiti.api.process.model.BPMNActivity;
 import org.activiti.api.process.model.BPMNSequenceFlow;
@@ -98,6 +100,9 @@ public class ProcessEngineEventsAggregator extends BaseCommandContextEventsAggre
             if (executionEntity != null) {
                 commandContext.addAttribute(executionId,
                                             executionContext);
+                
+                mayBeAddRootExecutionContext(commandContext, 
+                                             executionEntity);
             }
         }
         
@@ -105,6 +110,20 @@ public class ProcessEngineEventsAggregator extends BaseCommandContextEventsAggre
        
     }
     
+    protected void mayBeAddRootExecutionContext(CommandContext commandContext, ExecutionEntity executionEntity) {
+        ExecutionContext rootExecutionContext = commandContext.getGenericAttribute(ROOT_EXECUTION_CONTEXT);
+        
+        if(rootExecutionContext == null && executionEntity.getRootProcessInstanceId() != null) {
+            ExecutionEntity rootProcessInstance = commandContext.getExecutionEntityManager()
+                                                                .findById(executionEntity.getRootProcessInstanceId());
+            
+            rootExecutionContext = createExecutionContext(rootProcessInstance);
+
+            commandContext.addAttribute("rootExecutionContext",
+                                        rootExecutionContext);
+        }
+    }
+
     
     protected ExecutionContextInfoAppender createExecutionContextInfoAppender(ExecutionContext executionContext) {
         return new ExecutionContextInfoAppender(executionContext);
@@ -132,8 +151,7 @@ public class ProcessEngineEventsAggregator extends BaseCommandContextEventsAggre
         } else if(element instanceof CloudTaskCandidateGroupEvent) {
             return ((CloudTaskCandidateGroupEvent) element).getProcessInstanceId();
         } else if(element instanceof CloudBPMNSignalEvent) {
-            return ((CloudBPMNSignalEvent) element).getEntity().getProcessInstanceId();
-        } else if(element instanceof CloudBPMNTimerEvent) {
+            return ((CloudBPMNSignalEvent) element).getEntity().getProcessInstanceId();        } else if(element instanceof CloudBPMNTimerEvent) {
             return ((CloudBPMNTimerEvent) element).getEntity().getProcessInstanceId();
         } else if(element instanceof CloudBPMNMessageEvent) {
             return ((CloudBPMNMessageEvent) element).getEntity().getProcessInstanceId();
