efbfa56778c055ea6bcb50d4452b70fb07d007a4
Zoltan Palfi <zoltan.palfi@alfresco.com>
2020-02-25 10:48:15 +0000

    refactor: improve swagger test, cleanups
    part of AAE-2024

---
+++ activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/config/RBTestConfiguration.java
@@ -0,0 +1,15 @@
+package org.activiti.cloud.starter.tests.config;
+
+import org.activiti.spring.ProcessDeployedEventProducer;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("test")
+@Configuration
+public class RBTestConfiguration {
+
+    @MockBean
+    private ProcessDeployedEventProducer producer;
+
+}
--- activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/swagger/SwaggerSanityIT.java
+++ activiti-cloud-starter-runtime-bundle/src/test/java/org/activiti/cloud/starter/tests/swagger/SwaggerSanityIT.java
@@ -17,12 +17,10 @@
 package org.activiti.cloud.starter.tests.swagger;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
-import java.io.File;
-import java.nio.file.Files;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,28 +35,20 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
-public class SwaggerITSupport {
+public class SwaggerSanityIT {
 
     @Autowired
     private MockMvc mockMvc;
 
-    @Autowired
-    private ObjectMapper objectMapper;
-
-    /**
-     * This is not a test. It's actually generating the swagger.json and yaml definition of the service.
-     * It is used by maven generate-swagger profile build.
-     */
     @Test
-    public void generateSwagger() throws Exception {
+    public void should_swaggerDefinitionHavePathsAndDefinitionsAndInfo() throws Exception {
         mockMvc.perform(get("/v2/api-docs").accept(MediaType.APPLICATION_JSON))
-            .andDo((result) -> {
-                JsonNode jsonNodeTree = objectMapper.readTree(result.getResponse().getContentAsByteArray());
-                Files.write(new File("target/swagger.json").toPath(),
-                    objectMapper.writerWithDefaultPrettyPrinter().writeValueAsBytes(jsonNodeTree));
-                Files.write(new File("target/swagger.yaml").toPath(),
-                    new YAMLMapper().writeValueAsBytes(jsonNodeTree));
-            });
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+            .andExpect(jsonPath("$.paths").isNotEmpty())
+            .andExpect(jsonPath("$.definitions").isNotEmpty())
+            .andExpect(jsonPath("$.info.title").value("Activiti Cloud Starter :: Runtime Bundle ReST API"));
+
     }
 
 }
